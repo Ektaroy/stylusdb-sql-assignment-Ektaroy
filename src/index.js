66d9135184d3ca030ae1f639695db5398c6a2912
Ektaroy <97738792+Ektaroy@users.noreply.github.com>
2024-04-26 19:33:10 +0530

    feat: step 8

--- src/index.js
+++ src/index.js
@@ -2,12 +2,35 @@
 const parseQuery = require('./queryParser');
 const readCSV = require('./csvReader');
 
-async function executeSELECTQuery(query) {
-    const { fields, table, whereClauses } = parseQuery(query);
-    const data = await readCSV(`${table}.csv`);
 
-    // Apply WHERE clause filtering
-    const filteredData = whereClauses.length > 0
+
+async function executeSELECTQuery(query) {
+  
+// Now we will have joinTable, joinCondition in the parsed query
+const { fields, table, whereClauses, joinTable, joinCondition } = parseQuery(query);
+let data = await readCSV(`${table}.csv`);
+
+// Perform INNER JOIN if specified
+if (joinTable && joinCondition) {
+    const joinData = await readCSV(`${joinTable}.csv`);
+    data = data.flatMap(mainRow => {
+        return joinData
+            .filter(joinRow => {
+                const mainValue = mainRow[joinCondition.left.split('.')[1]];
+                const joinValue = joinRow[joinCondition.right.split('.')[1]];
+                return mainValue === joinValue;
+            })
+            .map(joinRow => {
+                return fields.reduce((acc, field) => {
+                    const [tableName, fieldName] = field.split('.');
+                    acc[field] = tableName === table ? mainRow[fieldName] : joinRow[fieldName];
+                    return acc;
+                }, {});
+            });
+    });
+}
+
+const filteredData = whereClauses.length > 0
     ? data.filter(row => whereClauses.every(clause => evaluateCondition(row, clause)))
     : data;
 
@@ -20,6 +43,9 @@ async function executeSELECTQuery(query) {
         return selectedRow;
     });
 }
+
+
+
 
 function evaluateCondition(row, clause) {
     const { field, operator, value } = clause;
@@ -35,3 +61,4 @@ function evaluateCondition(row, clause) {
 }
 
 module.exports = executeSELECTQuery;
+
